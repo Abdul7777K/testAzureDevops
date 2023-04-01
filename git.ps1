a8bacd8e5e51b4fac08b952ca549dcc3fea341b6
Abdul7777K <123536625+Abdul7777K@users.noreply.github.com>
2023-04-01 18:37:30 +0530

    Update git.ps1

--- git.ps1
+++ git.ps1
@@ -1,4 +1,4 @@
-$AZUREPAT = "65gbz645fntfpu6leg4cmvkspe4lgg4xfxodxtplvfc6yg35ezua"
+$AZUREPAT = "Ptw8Q~dJm7VV4VomdEMfWDCDZR1EPJ5f0-wRyc~f"
 $AZUSERNAME = "anything"
 $AZUSER_EMAIL = "nageshdevops77@outlook.com"
 $AZORG = "Learning-Devops-7777"
@@ -11,7 +11,7 @@ Remove-Item -Recurse -Force .git
 cd ..
 
 # Clone the repository again, this time with Azure credentials
-$GIT_CMD_REPOSITORY="https://uuxlmke3sjxili4cn47cn6wnri576uh2do2xj72yewnqxieqmeca@dev.azure.com/Learning-Devops-7777/Abdul/_git/syncWithgitEshopCommerce"
+$GIT_CMD_REPOSITORY="https://Ptw8Q~dJm7VV4VomdEMfWDCDZR1EPJ5f0-wRyc~f@dev.azure.com/Learning-Devops-7777/Abdul/_git/syncWithgitEshopCommerce"
 git clone $GIT_CMD_REPOSITORY
 
 # Copy the contents of the local repository to the cloned Azure repository
