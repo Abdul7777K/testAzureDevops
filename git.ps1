d8912a8c955de26ace5b145a7bf66f5834b228f0
Abdul7777K <123536625+Abdul7777K@users.noreply.github.com>
2023-04-01 18:08:21 +0530

    Add files via upload

---
+++ git.ps1
@@ -0,0 +1,31 @@
+$AZUREPAT = "bebi2dxrlnc3ixuoy6g43ozgn42dionf3uuo5mwhdktir4awr4na"
+$AZUSERNAME = "anything"
+$AZUSER_EMAIL = "nageshdevops77@outlook.com"
+$AZORG = "Learning-Devops-7777"
+
+# Clone the repository and remove its Git history
+git clone https://github.com/Abdul7777K/testAzureDevops.git
+cd testAzureDevops
+Remove-Item -Recurse -Force .git
+
+cd ..
+
+# Clone the repository again, this time with Azure credentials
+$GIT_CMD_REPOSITORY="https://bebi2dxrlnc3ixuoy6g43ozgn42dionf3uuo5mwhdktir4awr4na@dev.azure.com/Learning-Devops-7777/Abdul/_git/syncWithgitEshopCommerce"
+git clone $GIT_CMD_REPOSITORY
+
+# Copy the contents of the local repository to the cloned Azure repository
+Copy-Item -Recurse testAzureDevops/* syncWithgitEshopCommerce/
+
+cd syncWithgitEshopCommerce
+
+# Configure Git with Azure credentials
+git config --global user.email "$AZUSER_EMAIL"
+git config --global user.name "$AZUSERNAME"
+
+# Commit and push changes to Azure repository
+git add .
+git commit -m "sync from git to azure"
+git push
+
+
